f730bd79a29a17b39bb4068c0e9a548b308ececc
MaximShaybakov <momon_boton@mail.ru>
2022-06-30 07:13:36 +0500

    add requirenents.txt

--- some_func.py
+++ some_func.py
@@ -1,7 +1,10 @@
+import os.path
 from decorators.decorator1 import logger, logger_with_path
 
+path = os.path.abspath('generator.py')
+
 # @logger
-@logger_with_path(path='/home/maxim/git_projects/decors/logs/info_1.log')
+@logger_with_path(path=f'{os.path.dirname(path)}/logs/info_1.log')
 def some_func(x: int, y: int):
     return f'{x} * {y} = {x * y}'
 
